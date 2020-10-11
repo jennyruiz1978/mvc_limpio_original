125b4d972ddfa0f876041471eef184ef0cbc9381
Jenny Ruiz <53064299+jennyruiz1978@users.noreply.github.com>
2020-10-11 19:44:16 +0200

    Update generarPdf.php

--- app/librerias/generarPdf.php
+++ app/librerias/generarPdf.php
@@ -17,16 +17,19 @@ class generarPdf {
         $html2pdf->writeHTML($html);
         $html2pdf->output('ejemplo.pdf');
     }
-    public static function documentoPDFDesdevariable($orientacion,$formato,$idioma,$unicode,
-            $codificacion,$margenes, $pdfa, $variable, $salida) {
+    
 
+    public static function documentoPDFParaEmail($orientacion,$formato,$idioma,$unicode,
+        $codificacion,$margenes, $pdfa, $rutaCarpetasViews, $fichero, $datos='', $doc='') {
+            
         ob_start();
-        $variable;
+        include_once '../app/views/' . $rutaCarpetasViews . '/' . $fichero;
 
-        $html = ob_get_clean();
+        $html = ob_get_clean();    
         $html2pdf = new Html2Pdf($orientacion,$formato,$idioma,$unicode,$codificacion,$margenes,$pdfa);
-        $html2pdf->writeHTML($html);
-        $html2pdf->output($salida);
+        $html2pdf->writeHTML($html);        
+        $attachment = $html2pdf->output('factura.pdf', $doc);
+        return $attachment;
     }
 
 
