aead6bda11ab1b59bcdb1c61fc3d4798b363113a
Jenny Ruiz <53064299+jennyruiz1978@users.noreply.github.com>
2020-10-11 19:44:22 +0200

    Create UpDownLoadFichero.php

---
+++ app/librerias/UpDownLoadFichero.php
@@ -0,0 +1,53 @@
+<?php
+
+    /**
+     * Clase UpDownLoadFichero :
+     * 
+     * Clase para la subida o descarga de ficheros
+     * 
+     * @autor Jenny Ruiz
+     * 
+     * @version 1.0 - Versión inicial - Octubre 10 de 2020
+     * 
+     * @copyright Data Lean Makers
+     * 
+     */
+
+
+    class UpDownLoadFichero {
+
+        /**
+         * Función para la subida de un fichero
+         * 
+         * @return boolean true si el fichero se hasubido con éxito
+         * 
+         */
+        public static function uploadFile($subDirectorio,$nombre,$nombreTmp){
+
+            //DOCUMENTOS_PRIVADOS es una constante global definida en el config
+            if(!file_exists(DOCUMENTOS_PRIVADOS.$subDirectorio)){
+                mkdir(DOCUMENTOS_PRIVADOS.$subDirectorio,0777,true);
+                    if(file_exists(DOCUMENTOS_PRIVADOS.$subDirectorio)){
+                        if(move_uploaded_file($nombreTmp, DOCUMENTOS_PRIVADOS.$subDirectorio.'/'.$nombre)){
+                            //echo "Archivo guardado con exito";
+                            $upload = true;                        
+                        }else{
+                            $upload = false;                        
+                            //echo "Archivo no se pudo guardar";
+                        }
+                    }
+            }else{
+                if(move_uploaded_file($nombreTmp, DOCUMENTOS_PRIVADOS.$subDirectorio.'/'.$nombre)){
+                    $upload = true;                
+                    //echo "Archivo guardado con exito";
+                }else{
+                    $upload = false;                
+                    //echo "Archivo no se pudo guardar";
+                }
+            }
+            
+            return $upload;
+        }
+
+          
+    }
