629250895c298c510c0b1e8792d7292d328574d6
Jenny Ruiz <53064299+jennyruiz1978@users.noreply.github.com>
2020-10-11 19:43:03 +0200

    Update configurar.php

--- app/config/configurar.php
+++ app/config/configurar.php
@@ -1,9 +1,9 @@
 <?php
 // Configuracion de acceso a la base de datos
 define('DB_HOST','185.92.246.83');
-define('DB_USUARIO','informa');
-define('DB_PASSWORD','Informa2020$');
-define('DB_NOMBRE','admin_informa');
+define('DB_USUARIO','admin_mvc');
+define('DB_PASSWORD','admin_mvc2020$');
+define('DB_NOMBRE','admin_mvc');
 
 // Ruta de la aplicacion
 define('RUTA_APP', dirname(dirname(__FILE__)));
@@ -11,3 +11,6 @@ define('RUTA_APP', dirname(dirname(__FILE__)));
 define('RUTA_URL','http://localhost/mvc_limpio_original');
 // NOMBRE DEL SITIO
 define('NOMBRE_SITIO', 'MVC ORIGINAL');
+
+//Ruta para subida de ficheros:
+define("DOCUMENTOS_PRIVADOS", RUTA_APP."/documentos/");
